9e0c13b41a94a395be02f5ccaba569d020d6b53d
Dominik Dassow <dominik@dassow-net.de>
2019-09-15 18:27:13 +0200

    Update gulp and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var path = require('path');
 var gulp = require('gulp');
-var cleancss = require('gulp-clean-css');
-var rename = require("gulp-rename");
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
-gulp.task('default', ['scripts', 'sass'], function () {
-});
+var paths = {
+    scripts: 'src/js/*.js',
+    sass: 'src/sass/*.scss'
+};
 
-gulp.task('scripts', function () {
-    return gulp.src('src/js/*.js')
+function scriptsTask(done) {
+    gulp.src(paths.scripts)
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
-});
 
-gulp.task('sass', function () {
-    // compile sass files
-    return gulp.src('src/sass/*.scss')
+    done();
+}
+
+function sassTask(done) {
+    gulp.src(paths.sass)
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
@@ -29,10 +29,18 @@ gulp.task('sass', function () {
         .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
-});
 
-// watchers
-gulp.task('watch', function () {
-    gulp.watch('src/js/*.js', ['scripts']);
-    gulp.watch('src/sass/*.scss', ['sass']);
-});
+    done();
+}
+
+function watch(done) {
+    gulp.watch(paths.scripts, scriptsTask);
+    gulp.watch(paths.sass, sassTask);
+
+    done();
+}
+
+exports.scripts = scriptsTask;
+exports.sass = sassTask;
+exports.watch = watch;
+exports.default = gulp.series(scriptsTask, sassTask);
